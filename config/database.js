8bbff1d910f911cd249a0407d4b8dc628d4522ae
ayushmittal007 <ayushagrawal102004@gmail.com>
2023-12-22 01:12:26 +0530

    changed db connections

--- config/database.js
+++ config/database.js
@@ -2,10 +2,25 @@ const Sequelize = require('sequelize');
 const pg = require('pg');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,{
-    logging:false
-});
+// const sequelize = new Sequelize(process.env.,{
+    // logging:false
+// });
 
+// const sequelize = new Sequelize(process.env.DATABASE_URL,{
+    // logging:false
+// });
+
+const sequelize = new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      host: process.env.DATABASE_HOST,
+      dialect: 'postgres',
+      logging: false,
+    }
+  );
+  
 module.exports = {
     sequelize
 };
