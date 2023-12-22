b2e643a3e93a8816649cbb4d27ae66eb4c88862c
ayushmittal007 <ayushagrawal102004@gmail.com>
2023-12-22 17:07:40 +0530

    added postgres db conn string

--- config/database.js
+++ config/database.js
@@ -3,24 +3,8 @@ const pg = require('pg');
 require('dotenv').config();
 
 
-const sequelize = new Sequelize(
-    process.env.DATABASE_NAME,
-    process.env.DATABASE_USER,
-    process.env.DATABASE_PASSWORD,
-    {
-      host: process.env.DATABASE_HOST,
-      dialect: 'postgres',
-      logging: false,
-    }
-  );
-  
-sequelize
-.authenticate()
-.then(() => {
-console.log('Connection to the database has been established successfully.');
-})
-.catch((err) => {
-console.error('Unable to connect to the database:', err);
+const sequelize = new Sequelize(process.env.DATABASE_URL,{
+    logging:false
 });
 
 module.exports = {
